3e5f7690aff11bfc81bf3668a7858191a3fb2da0
Mauricio Mucci <57639887+MauricioMucci@users.noreply.github.com>
2025-12-03 20:18:15 -0300

    feat: add back-to-top button and smooth scrolling functionality

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function scroll(event) {
+  event.preventDefault();
+  const targetId = event.currentTarget.getAttribute("href");
+  const targetElement = document.querySelector(targetId);
+  targetElement.scrollIntoView({ behavior: "smooth" });
+}
+
+function scrollToTop(event) {
+  event.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+const backgroundLink = document.querySelector("#background-link");
+if (backgroundLink) {
+  backgroundLink.addEventListener("click", scroll);
+}
+
+const skillsLink = document.querySelector("#skills-link");
+if (skillsLink) {
+  skillsLink.addEventListener("click", scroll);
+}
+
+const contactLink = document.querySelector("#contact-link");
+if (contactLink) {
+  contactLink.addEventListener("click", scroll);
+}
+
+const backToTop = document.querySelector("#back-to-top");
+if (backToTop) {
+  backToTop.addEventListener("click", scrollToTop);
+}
